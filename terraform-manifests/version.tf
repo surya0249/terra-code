bfd5e1ad20d2cf84c5ac1da5ea813d07de395b4b
surya0249 <sajja.surya2011@gmail.com>
2021-06-01 14:57:43 +0530

    jenkinsfile

--- terraform-manifests/version.tf
+++ terraform-manifests/version.tf
@@ -14,3 +14,14 @@ provider "aws" {
   region  = var.aws_region
   profile = "default"
 }
+
+
+# Adding Backend as S3 for Remote State Storage with State Locking
+  backend "s3" {
+    bucket = "terracode-2021"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-2"  
+
+    # For State Locking
+    dynamodb_table = "terraform"
+  }
